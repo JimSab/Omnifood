55e6493858e14392bdf85542dcfa478329ac6806
Jimsab <jamesbayega@gmail.com>
2020-07-13 05:52:19 +0300

    Added some jquery

---
+++ resources/js/script.js
@@ -0,0 +1,13 @@
+/*jslint browser: true*/
+/*global $, jQuery*/
+
+
+$(document).ready(function () {
+    
+    
+    $('h1').click(function () {
+        
+        $(this).css('background-color', '#ff0000');
+    }); 
+    
+});
